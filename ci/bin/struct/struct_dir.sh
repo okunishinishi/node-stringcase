5093411f266dd4f91eeb539f5f6faa54a93303cb
Taka Okunishi <okunishinishi@gmail.com>
2015-07-11 15:18:23 +0900

    add coverage script

--- ci/bin/struct/struct_dir.sh
+++ ci/bin/struct/struct_dir.sh
@@ -11,10 +11,16 @@ cd ${BASE_DIR}
 
 mkdir -p ci
 mkdir -p ci/bin
+mkdir -p ci/bin/cover
+mkdir -p ci/bin/deploy
+mkdir -p ci/bin/doc
+mkdir -p ci/bin/report
+mkdir -p ci/bin/render
 mkdir -p ci/bin/struct
-mkdir -p ci/bin/compile
+mkdir -p ci/bin/tag
 mkdir -p ci/bin/test
 mkdir -p docs
+mkdir -p docs/reports
 mkdir -p lib
 mkdir -p test
 mkdir -p test/unit_tests
--- test/unit_tests/trimcase_test.js
+++ test/unit_tests/trimcase_test.js
@@ -7,6 +7,7 @@ var trimcase = require('../../lib/trimcase.js');
 
 exports['Trimcase'] = function(test){
     test.equal(trimcase(' foo bar baz '), 'foo bar baz');
+    test.equal(trimcase(''), '');
     test.done();
 };
 
