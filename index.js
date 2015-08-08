d8e673b49e0d8f98dcde317c5b9d33a60c92b2ed
Taka Okunishi <okunishinishi@gmail.com>
2015-08-08 17:41:51 +0900

    Version incremented to 1.1.2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @overview Convert string cases between camel case, pascal case, snake case etc...
- * @version 1.1.0
+ * @version 1.1.1
  * @module stringcase
  * @author {@link http://okunishitaka.com|Taka Okunishi}
  * @see https://github.com/okunishinishi/node-stringcase#readme
