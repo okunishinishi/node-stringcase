0ca79a89ab5c926a3546ee577a231d56cb6c0957
Taka Okunishi <okunishinishi@gmail.com>
2015-08-09 12:04:31 +0900

    Version incremented to 1.2.4

--- test/titlecase_test.js
+++ test/titlecase_test.js
@@ -11,6 +11,7 @@ var titlecase = require('../lib/titlecase.js');
 exports['Convert to camel case.'] = function (test) {
     test.equal(titlecase('fooBar'), 'Foo Bar');
     test.equal(titlecase('foo_bar'), 'Foo Bar');
+    test.equal(titlecase('foo_for_bar'), 'Foo for Bar');
     test.equal(titlecase('foo-bar'), 'Foo Bar');
     test.equal(titlecase('foo.bar'), 'Foo Bar');
     test.equal(titlecase('_bar_baz'), ' Bar Baz');
